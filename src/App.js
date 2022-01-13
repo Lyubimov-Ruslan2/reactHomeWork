bcbbf567b6ed0e13eeb8eb2b3e96ba92b90a5fdc
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2022-01-13 18:52:37 +0500

    I thinks, It's the last commit for today

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-import Hello from './components/Helloworld';
-import IncDecBtn from './components/incrementDecrement';
+import Hello from './components/Hello/Helloworld'
+import IncDecBtn from './components/Buttons/incrementDecrement';
+import SearchBar from './components/SearchBar/searchBar';
+import ToggleGenre from './components/Togglegenre/Togglegenre';
 function App() {
   return (
     <div className="App">
-      <IncDecBtn/>
-      <Hello/>
+       {Hello}
+      <IncDecBtn />
+      <SearchBar/>
+      <ToggleGenre/>
+     
     </div>
   );
 }
-
 export default App;
