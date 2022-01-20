615bf6e5ae6d7e1ce9c228254feb1f0274fa4664
Ruslan Lyubimov <ruslan_lyubimov@epam.com>
2022-01-20 17:20:03 +0500

    Last, but not least

--- src/components/MovieContainer/MovieContainer.component.jsx
+++ src/components/MovieContainer/MovieContainer.component.jsx
@@ -14,36 +14,42 @@ let movies = [
     img: logo1,
     year: 2004,
     genre: "Action & Adventure",
+    key: "asdaf",
   },
   {
     title: "Bohemian Rhapsody",
     img: logo2,
     year: 2003,
     genre: "Drama, Biography, Music",
+    key: "asdaffs",
   },
   {
     title: "Kill Bill: Vol 2",
     img: logo3,
     year: 1994,
     genre: "Oscar winning Movie",
+    key: "asdafwq",
   },
   {
     title: "Avengers: War of Infinity",
     img: logo4,
     year: 2004,
     genre: "Action & Adventure",
+    key: "asdafuit",
   },
   {
     title: "Inception",
     img: logo5,
     year: 2003,
     genre: "Action & Adventure",
+    key: "asdafcasd",
   },
   {
     title: "Reservoir dogs",
     img: logo6,
     year: 1994,
     genre: "Oscar winning Movie",
+    key: "asdafyluyl",
   },
 ];
 
@@ -52,8 +58,8 @@ const MovieContainer = () => {
     <>
       <p className="found">{movies.length} movies found</p>
       <div className="movie-list">
-        {movies.map((movie) => (
-          <Movie movie={movie} />
+        {movies.map(({ title, img, year, genre, key }) => (
+          <Movie title={title} img={img} year={year} genre={genre} key={key} />
         ))}
       </div>
     </>
--- src/components/movie/movie.component.jsx
+++ src/components/movie/movie.component.jsx
@@ -1,17 +1,31 @@
 import react from "react";
+import PropTypes from "prop-types";
 import "./movie.styles.css";
 
-const Movie = (props) => {
+const Movie = ({ img, title, year, genre, key }) => {
   return (
-    <div className="movie-about">
-      <img src={props.movie.img} />
+    <div key={key} className="movie-about">
+      <img src={img} alt={`${(key = { key })}`} />
       <div className="about">
-        <p>{props.movie.title}</p>
-        <button className="btn-about">{props.movie.year}</button>
+        <p>{title}</p>
+        <button className="btn-about">{year}</button>
       </div>
-      <p className="genre-about">{props.movie.genre}</p>
+      <p className="genre-about">{genre}</p>
     </div>
   );
+};
+
+Movie.defaultProps = {
+  title: "Amazing movie!",
+  year: 2000,
+  genre: "Action & Adventure",
+};
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  genre: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  key: PropTypes.string.isRequired,
 };
 
 export default Movie;
